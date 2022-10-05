233ae2ecacfabe122828c66dfdef6ebd6af1eb3a
Aleksey Yaroslavcev <a.yaroslavcev@gmail.com>
2022-10-05 22:25:16 +0300

    release 1

--- ectocontrol-avr-sensors.ino
+++ ectocontrol-avr-sensors.ino
@@ -5,30 +5,49 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
-const int numHR = 4;  // 4 general registers
-const int numIR = 10; // 10 sensors data
-const byte defaultAddr = 0xF0;
-byte curAddr = 0x00;
-const word rs485Speed = 19200;
 const byte sensType = 0x22; // 0x22 - temp, 0x23 - humidity
 const int sensorsCount = 5;
 DHT_Unified* dht[sensorsCount];
-Modbus bus(curAddr,Serial,2);
+
+const int ledPin = 7;
+
+const int butPin = 6;
+
+const byte defaultAddr = 0xF0;
+byte curAddr = 0x00;
+const word rs485Speed = 19200;
+const int rs485pin = 2;
+Modbus bus(curAddr, Serial, rs485pin);
+const int numHR = 4;  // 4 general registers
+const int numIR = 10; // 10 sensors data
 uint16_t modbusData[numHR + numIR];
 
+void blinkLed(int cnt = 1) {
+  for(int i = 0; i < cnt; ++i) {
+    digitalWrite (ledPin, HIGH);
+    delay(50);
+    digitalWrite (ledPin, LOW);
+    delay(50);
+  }
+}
+
 void setup() {
-  Serial.begin(19200);
+  Serial.begin(rs485Speed);
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite (ledPin, LOW);
+
+  if(digitalRead(butPin)) {
+    blinkLed(5);
+    EEPROM.put(0, defaultAddr);
+  }
 
   for(int i = 0; i < sensorsCount; ++i){
-    dht[i] = new DHT_Unified(i+9, DHT11);
+    dht[i] = new DHT_Unified(i + 8, DHT11);
     dht[i]->begin();
   }
 
   EEPROM.get(0, curAddr);
-  if (curAddr == 0) {
-    curAddr = defaultAddr;
-  }
-  EEPROM.put(0, curAddr);
   bus.setID(curAddr);
   bus.start();
 
@@ -58,7 +77,10 @@ void updateSensorsData() {
 }
 
 void loop() {
-  bus.poll(modbusData, numHR + numIR);
+  int8_t state = bus.poll(modbusData, numHR + numIR);
+  if(state > 4) {
+    blinkLed();
+  }
 
   checkAddr();
 
